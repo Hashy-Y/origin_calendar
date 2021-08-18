46e3a107caa31354a4a047a16d115ca697b3e0a3
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-18 23:16:04 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true } do
+      validates :nickname
+    end
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, allow_blank: true } do
+    validates :password
+    validates :password_confirmation
+  end
 end
