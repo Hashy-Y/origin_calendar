1838de349639356ecfecade06dbd03588df96ef9
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-19 03:16:19 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  with_options presence: true do
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true } do
-      validates :nickname
-    end
-  end
+  validates :nickname, presence: true
+  #with_options presence: true do
+    #with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true } do
+      #validates :nickname
+    #end
+  #end
 
-  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, allow_blank: true } do
-    validates :password
-    validates :password_confirmation
-  end
+  #with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, allow_blank: true } do
+    #validates :password
+    #validates :password_confirmation
+  #end
 end
