858e2ffca2b1fc0683504cc4dfbedaf2e52d61b8
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-20 00:06:55 +0900

    バリデーションの設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  has_many :room_users
+  has_many :users, through: :group_users
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  has_many :group_users
+  has_many :groups, through: :group_users
+
   validates :nickname, presence: true
 
   with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i} do
