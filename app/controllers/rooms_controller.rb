6ef9a4df571af6e015028774e4f6b5d29e4dee40
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-20 00:28:14 +0900

    コントローラーの記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,20 @@
 class RoomsController < ApplicationController
+  def new
+    @room = room.new
+    @room.users << current_user
+  end
+
+  def create
+    @room = room.new(room_params)
+    if @room.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [] )
+  end
 end
