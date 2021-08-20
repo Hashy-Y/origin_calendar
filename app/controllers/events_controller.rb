9e5790b23328f26e49755d8c205b93a78cba42c3
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-21 01:16:33 +0900

    ルートとコントローラーの設定

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,52 @@
 class EventsController < ApplicationController
   def index
+    @event = Event.new
+    @room = Room.find(params[:room_id])
+    @events = @room.events.includes(:user)
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @event = @room.events.new(events_params)
+    if @events.save
+      redirect_to room_events_path(@room)
+    else
+      @events = @room.events.includes(:user)
+      render :new
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, notice:"削除しました"
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to events_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content, :start_time).merge(user_id: current_user.id)
+  end
+
 end
