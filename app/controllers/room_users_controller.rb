e68fdf94ea548e094f78946b9041ff990331f9ea
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-08-25 12:37:20 +0900

    ルーム参加者の一覧表示機能実装

--- app/controllers/room_users_controller.rb
+++ app/controllers/room_users_controller.rb
@@ -1,4 +1,10 @@
 class RoomUsersController < ApplicationController
+  
+  def index
+    @room = Room.find(params[:room_id])
+    @room_users = RoomUser.where(params[room_id: @room.id])
+  end
+
   def destroy
     @room = Room.find(params[:room_id])
     @room_user = RoomUser.find(params[:id])
